a38dbb7c51743c193dc8b00d31c02872dde5a923
Mustafa Haddara <mustafaayhaddara@yahoo.com>
2022-11-30 23:15:51 -0500

    set up code running harness

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,18 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+package adventofcode
 
-def msg = "I was compiled by Scala 3. :)"
+import scala.io.StdIn.readLine
+import scala.io.Source
+import solutions.Day
+
+val solvers: Vector[Day] = Vector()
+
+@main def main(dayNum: Int, problem: String): Unit = 
+    println(s"running day $dayNum problem $problem")
+
+    val input = Source.fromFile(f"inputs/$dayNum%02d.txt").getLines.toList
+
+    val solver = solvers(dayNum - 1)
+    problem match
+        case "A" => println(solver.solveA(input))
+        case "B" => println(solver.solveB(input))
+        case _ => println("Invalid problem given")
---
+++ src/main/scala/solutions/Day.scala
@@ -0,0 +1,5 @@
+package solutions
+
+trait Day:
+    def solveA(input: List[String]): String
+    def solveB(input: List[String]): String
