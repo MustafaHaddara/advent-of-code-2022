f87f1958bd7752e569fa117c27e3aefcf2e92bd4
Mustafa Haddara <mustafaayhaddara@yahoo.com>
2022-12-03 13:42:43 -0500

    scalafmt

--- src/main/scala/solutions/Day.scala
+++ src/main/scala/solutions/Day.scala
@@ -1,5 +1,7 @@
 package solutions
 
 trait Day:
-    def solveA(input: List[String]): String
-    def solveB(input: List[String]): String
+  val day: Int
+  val testInput: String
+  def solveA(input: List[String]): String
+  def solveB(input: List[String]): String
--- src/main/scala/solutions/Day01.scala
+++ src/main/scala/solutions/Day01.scala
@@ -4,28 +4,46 @@ import scala.collection.mutable.ArrayBuffer
 import solutions.Day
 
 class Day01 extends Day:
-    def solveA(input: List[String]): String =
-        val totals = ArrayBuffer[Int]()
-        var curr = 0
-        for (line <- input) {
-            if (line == "") {
-                totals += curr
-                curr = 0
-            } else {
-                curr += line.toInt
-            }
-        }
-        return totals.max.toString()
+  val day = 1;
+  val testInput = """
+1000
+2000
+3000
 
-    def solveB(input: List[String]): String =
-        val totals = ArrayBuffer[Int]()
-        var curr = 0
-        for (line <- input) {
-            if (line == "") {
-                totals += curr
-                curr = 0
-            } else {
-                curr += line.toInt
-            }
-        }
-        return totals.sortWith(_ > _).slice(0, 3).sum.toString()
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+
+  def solveA(input: List[String]): String =
+    val totals = ArrayBuffer[Int]()
+    var curr = 0
+    for (line <- input) {
+      if (line == "") {
+        totals += curr
+        curr = 0
+      } else {
+        curr += line.toInt
+      }
+    }
+    return totals.max.toString()
+
+  def solveB(input: List[String]): String =
+    val totals = ArrayBuffer[Int]()
+    var curr = 0
+    for (line <- input) {
+      if (line == "") {
+        totals += curr
+        curr = 0
+      } else {
+        curr += line.toInt
+      }
+    }
+    return totals.sortWith(_ > _).slice(0, 3).sum.toString()
